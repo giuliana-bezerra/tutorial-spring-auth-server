f6dc0eacd7990578fc8c358eff8b21195655479b
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2023-06-11 10:06:48 -0300

    Update Spring Boot version

--- auth-server/src/main/java/br/com/giulianabezerra/authserver/SecurityFilterConfig.java
+++ auth-server/src/main/java/br/com/giulianabezerra/authserver/SecurityFilterConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,12 +23,12 @@ public class SecurityFilterConfig {
   SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
-    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(withDefaults())
-        .and()
-        .exceptionHandling((exceptions) -> exceptions
-            .authenticationEntryPoint(
-                new LoginUrlAuthenticationEntryPoint("/login")))
-        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(withDefaults());
+
+    http.exceptionHandling((exceptions) -> exceptions
+        .authenticationEntryPoint(
+            new LoginUrlAuthenticationEntryPoint("/login")))
+        .oauth2ResourceServer(conf -> conf.jwt(withDefaults()));
 
     return http.build();
   }
@@ -49,4 +48,5 @@ public class SecurityFilterConfig {
 
     return http.build();
   }
+
 }
